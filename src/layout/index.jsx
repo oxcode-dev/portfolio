6a61169ac0a617a70904c1454fc25443e3bfebac
oxcode <mrexcelsam1@live.com>
2024-05-12 15:57:11 +0100

    Home Hero View

--- src/layout/index.jsx
+++ src/layout/index.jsx
@@ -9,7 +9,7 @@ export default function RootLayout({children}) {
         <>
             <Head>
                 <title>Samuel Osemeke</title>
-                <Script src="https://cdn.tailwindcss.com" defer></Script>
+                <script src="https://cdn.tailwindcss.com" defer></script>
                 <link rel="preconnect" href="https://fonts.googleapis.com" />
                 <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin='true' />
                 <link href="https://fonts.googleapis.com/css2?family=Domine:wght@400..700&family=Inter:slnt,wght@-10..0,100..900&display=swap" rel="stylesheet"></link>
