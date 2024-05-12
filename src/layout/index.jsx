1c775e3bec1829d6d69ff35787da17d9e9e3f970
oxcode <mrexcelsam1@live.com>
2024-05-12 15:40:27 +0100

    Home Hero View

--- src/layout/index.jsx
+++ src/layout/index.jsx
@@ -2,13 +2,14 @@
 
 import { NavBar } from "../components/index.js";
 import Head from "next/head.js";
+import Script from 'next/script'
 
 export default function RootLayout({children}) {
     return (
         <>
             <Head>
                 <title>Samuel Osemeke</title>
-                <script src="https://cdn.tailwindcss.com" />
+                <Script src="https://cdn.tailwindcss.com"></Script>
                 <link rel="preconnect" href="https://fonts.googleapis.com" />
                 <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin='true' />
                 <link href="https://fonts.googleapis.com/css2?family=Domine:wght@400..700&family=Inter:slnt,wght@-10..0,100..900&display=swap" rel="stylesheet"></link>
