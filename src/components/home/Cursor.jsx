de21bfe64542296e9276cd1b35c6f2cb2d3f00ac
oxcode <mrexcelsam1@live.com>
2024-06-08 00:41:41 +0100

    Navbar update

--- src/components/home/Cursor.jsx
+++ src/components/home/Cursor.jsx
@@ -26,8 +26,6 @@ export const Cursor = () => {
         <motion.div
             className="hidden md:inline-flex items-center justify-center rounded-full w-10 h-10 fixed z-[99999999] bg-white mix-blend-difference"
             animate={{ x: position.x+10, y: position.y+10 }}
-            >
-            {/* <span className="text-lg text-red-600">&hearts;</span> */}
-        </motion.div>
+        ></motion.div>
     );
 };
