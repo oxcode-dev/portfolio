2764ef6798ce8854eefda3b725cc4bf7799df5f7
oxcode <mrexcelsam1@live.com>
2024-06-07 09:17:52 +0100

    Navbar update

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -35,47 +35,46 @@ export const NavBar = () => {
                     </div>
                 </div>
             </div>
-            {
-                active &&  <div className="h-screen w-full fixed bg-[#060801] z-[70] top-0 left-0">
-                    <div className="w-full flex flex-col items-center justify-across h-full py-16 space-y-8">
-                        <div className="">
-                            <Logo />
-                        </div>
-                        <div className='absolute right-4 md:right-24 top-4'>
-                            <a href="#" onClick={e => toggleNavs(e)}>
-                                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={2} stroke="currentColor" className="w-6 h-6">
-                                    <path strokeLinecap="round" strokeLinejoin="round" d="M6 18 18 6M6 6l12 12" />
-                                </svg>
-                            </a>
-                        </div>
-                        <div className="py-10">
-                            <div className='w-full mx-auto h-full'>
-                                <div className='flex flex-col mx-auto items-center justify-center h-full'>
-                                    { navigationLinks.map((nav, key) => (
-                                        <a
-                                            onClick={() => setActive(prev => !prev)}
-                                            className={`px-4 py-5 text-2xl font-normal hover:text-gray-400`}
-                                            key={key} 
-                                            href={nav?.link || '#'}
-                                        >
-                                            {nav?.title || "N/A"}
-                                        </a>
-                                    ))}
-                                </div>
-                            </div>
-                        </div>
-                        <div className="py-2">
-                            <div className="space-x-4">
-                                {
-                                    socialLinks.map((social, key) => (
-                                        <a href={social?.link || '#'} key={key}>{social?.name || "N/A"}</a>
-                                    ))
-                                }
+            
+            <div className={`h-screen w-full fixed bg-[#060801] z-[70] top-0 left-0 transition-transform duration-[0.8s] ease-[ease-in-out] ${active ? "translate-y-[0%]" : "translate-y-[-150%]"}`}>
+                <div className="w-full flex flex-col items-center justify-across h-full py-16 space-y-8">
+                    <div className="">
+                        <Logo />
+                    </div>
+                    <div className='absolute right-4 md:right-24 top-4'>
+                        <a href="#" onClick={e => toggleNavs(e)}>
+                            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={2} stroke="currentColor" className="w-6 h-6">
+                                <path strokeLinecap="round" strokeLinejoin="round" d="M6 18 18 6M6 6l12 12" />
+                            </svg>
+                        </a>
+                    </div>
+                    <div className="py-10">
+                        <div className='w-full mx-auto h-full'>
+                            <div className='flex flex-col mx-auto items-center justify-center h-full'>
+                                { navigationLinks.map((nav, key) => (
+                                    <a
+                                        onClick={() => setActive(prev => !prev)}
+                                        className={`px-4 py-5 text-2xl font-normal hover:text-gray-400`}
+                                        key={key} 
+                                        href={nav?.link || '#'}
+                                    >
+                                        {nav?.title || "N/A"}
+                                    </a>
+                                ))}
                             </div>
                         </div>
                     </div>
+                    <div className="py-2">
+                        <div className="space-x-4">
+                            {
+                                socialLinks.map((social, key) => (
+                                    <a href={social?.link || '#'} key={key}>{social?.name || "N/A"}</a>
+                                ))
+                            }
+                        </div>
+                    </div>
                 </div>
-            }
+            </div>
         </>
     )
 }
