b5c737f8a61a714d10c827eea2e2f22b5979dca9
oxcode <mrexcelsam1@live.com>
2024-06-14 22:33:49 +0100

    Navbar update

--- src/components/home/About.jsx
+++ src/components/home/About.jsx
@@ -30,13 +30,13 @@ export const About = () => {
                                 I&apos;m also a strong advocate for web accessibility, ensuring that all my projects are accessible to all.
                             </p>
                         </div>
-                        <div class="mt-8 md:pl-4">
-                            <a class="inline-flex items-baseline leading-tight font-semibold text-slate-200 group group/link text-lg" href="/resume.pdf" target="_blank" rel="noreferrer noopener" aria-label="View Full Résumé (opens in a new tab)">
+                        <div className="mt-8 md:pl-4">
+                            <a className="inline-flex items-baseline leading-tight font-semibold text-slate-200 group group/link text-lg" href="/resume.pdf" target="_blank" rel="noreferrer noopener" aria-label="View Full Résumé (opens in a new tab)">
                                 <span className='space-x-1'>
                                     <span className='group-hover:underline'>View Full</span>
-                                    <span class="inline-block group-hover:underline">
+                                    <span className="inline-block group-hover:underline">
                                         Résumé
-                                        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" class="inline-block h-4 w-4 shrink-0 transition-transform group-hover/link:-translate-y-1 group-hover/link:translate-x-1 group-focus-visible/link:-translate-y-1 group-focus-visible/link:translate-x-1 motion-reduce:transition-none ml-1 translate-y-px" aria-hidden="true"><path fill-rule="evenodd" d="M5.22 14.78a.75.75 0 001.06 0l7.22-7.22v5.69a.75.75 0 001.5 0v-7.5a.75.75 0 00-.75-.75h-7.5a.75.75 0 000 1.5h5.69l-7.22 7.22a.75.75 0 000 1.06z" clipRule="evenodd"></path></svg>
+                                        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" className="inline-block h-4 w-4 shrink-0 transition-transform group-hover/link:-translate-y-1 group-hover/link:translate-x-1 group-focus-visible/link:-translate-y-1 group-focus-visible/link:translate-x-1 motion-reduce:transition-none ml-1 translate-y-px" aria-hidden="true"><path fill-rule="evenodd" d="M5.22 14.78a.75.75 0 001.06 0l7.22-7.22v5.69a.75.75 0 001.5 0v-7.5a.75.75 0 00-.75-.75h-7.5a.75.75 0 000 1.5h5.69l-7.22 7.22a.75.75 0 000 1.06z" clipRule="evenodd"></path></svg>
                                     </span>
                                 </span>
                             </a>
