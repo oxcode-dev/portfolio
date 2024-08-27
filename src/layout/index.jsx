971e28e2d3eb9d70b6c1897d8993e6fc2244b1d8
oxcode <mrexcelsam1@live.com>
2024-08-27 17:43:43 +0100

    Navbar update

--- src/layout/index.jsx
+++ src/layout/index.jsx
@@ -8,10 +8,10 @@ import Script from 'next/script'
 import { Metadata } from 'next'
 
 export const metadata = {
-  title: {
-    template: '%s | Acme',
-    default: 'Acme', // a default is required when creating a template
-  },
+    title: {
+        template: '%s | Acme',
+        default: 'Acme', // a default is required when creating a template
+    },
 }
 
 export default function RootLayout({children}) {
