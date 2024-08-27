c8185ef97c3ac352dc905a267abfcafd69e589a8
oxcode <mrexcelsam1@live.com>
2024-08-27 18:01:49 +0100

    Navbar update

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -5,6 +5,7 @@ import Layout from '../layout'
 export default function Home() {
   return (
       <Layout>
+        Hello World
         <HomeHero />
         <HomeAbout />
         <HomeProjects />
