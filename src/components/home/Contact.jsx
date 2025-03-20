a4067acb3cd74978e92297b3a2bb3d0a30a5b812
oxcode <mrexcelsam1@live.com>
2025-03-20 19:44:31 +0100

    Navbar update

--- src/components/home/Contact.jsx
+++ src/components/home/Contact.jsx
@@ -13,7 +13,7 @@ export const Contact = () => {
                 </div>
                 
                 <div className="relative bottom-0 w-full">
-                    <div className={`h-full w-full absolute bg-gradient-to-b from-[${isDarkMode ? '#060801' : 'white'}] to-black/50 via-[${isDarkMode ? '#060801' : 'white'}] via-40% `}>
+                    <div className={`h-full w-full absolute bg-gradient-to-b from-[${isDarkMode ? '#060801' : 'white'}] to-${isDarkMode ? 'black' : 'white'}/50 via-[${isDarkMode ? '#060801' : 'white'}] via-30% `}>
                         <div className='container mx-auto flex flex-col items-center px-6 sm:px-8 md:px-16 py-24 md:py-32 md:pt-48 relative z-10'>
                             <p className="text-md md:text-xl uppercase font-extralight">Thanks For stopping by.</p>
                             <div className="md:py-16 py-8 w-full md:max-w-5xl text-center">
